337e29f981959d60588dadc01dbd82620a6e6052
David Fandler <d.fandler@javiis.com>
2019-01-28 10:36:30 +0100

    first version

--- main.go
+++ main.go
@@ -4,17 +4,27 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func main() {
 	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
+
+	versioncode:= os.Getenv("input_version_code")
+	versionname:= os.Getenv("input_version_name")
+
+	t:= time.Now()
+	timeString:= t.Format("0601021504")
+
+	fmt.Println("input version code: 'versioncode' and versionname 'versionname'",versioncode,versionname)
+
 
 	//
 	// --- Step Outputs: Export Environment Variables for other Steps:
 	// You can export Environment Variables for other Steps with
 	//  envman, which is automatically installed by `bitrise setup`.
 	// A very simple example:
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
+	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "OUTPUT_VERSION_NAME", "--value", timeString).CombinedOutput()
 	if err != nil {
 		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
 		os.Exit(1)
